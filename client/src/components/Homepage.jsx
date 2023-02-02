a62d1db11f674c38e746b1cfd95d993bd8eae4b4
Schmeef <keifer.smith95@gmail.com>
2023-02-02 13:29:50 -0800

    final touches

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -8,8 +8,8 @@ const Homepage = () => {
         <div className='background'>
         <div className='container mx-auto d-flex justify-content-center mt-5'>
             <div className='text-success'>
-                <h1 className='bg-black'>Choose your favorite now!!</h1>
-                <Link className='bg-black text-success d-flex justify-content-center ' to={`/api/createDigimon/`}> Start here!</Link>
+                <h1 className='heading'>Choose your favorite now!!</h1>
+                <Link className='bigbutton mx-auto ' to={`/api/createDigimon/`}> Start here!</Link>
             </div>
         </div>
     </div>
